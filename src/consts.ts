0cebcda116183b15e3be1494d51374f4d5193108
Randall D. Johnson <nollidnosnhoj@gmail.com>
2024-11-17 20:05:18 -1000

    ,

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Randall D Johnson (kopacetic)",
+  NAME: "Randall D Johnson (nollidnosnhoj)",
   EMAIL: "nollidnosnhoj@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
